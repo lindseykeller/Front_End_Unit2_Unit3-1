20072dbf85cb6da44dbbf4e0a7debc789507449f
Lindsey Henager <lindseyhenager@lindseys-air.attlocal.net>
2020-09-21 17:17:58 -0700

    login and sign up

--- how-to/src/components/Login.js
+++ how-to/src/components/Login.js
@@ -1,102 +1,87 @@
 
-
-import React,{useState,useEffect} from 'react'
-import Dashboard from './Dashboard'
+import React, { useState } from "react";
+import axios from "axios";
 import *as yup from 'yup'
-import {Route,Link} from 'react-router-dom'
-import SignUp from './SignUp'
+import { GlobalContext } from "../context/GlobalContext";
 import styled from 'styled-components'
 
 const Group = styled.div`
 display:block`
 
+export default function AuthForm({ role, history }) {
+  const { setLoggedIn } = React.useContext(GlobalContext);
+
+  const [authInfo, setAuthInfo] = useState({
+    username: "",
+    password: "",
+  });
+//needed to change userName to username
+  const[errors,setErrors] = useState({
+
+    username:'',
+    password:'',
+    message:''
+})
+
+const logFormSchema = yup.object().shape({
+    userName: yup.string().required('must enter user name'),
+    password: yup.string().required('must enter password')
+})
 
 
+  const handleChange = (e) => {
+    setAuthInfo({
+      ...authInfo,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-const LogInForm =props=>{
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-    
-    const[users,setUsers]= useState([{
-       
-        name:'',
-        email:'',
-        userName:'',
-        password:'',
+    axios
+      .post(
+        `https://better-professor-build-week.herokuapp.com/auth/login`,
+        authInfo
+      )
+      .then((res) => {
+        console.log(res);
+        setLoggedIn(true);
+        localStorage.setItem("token", res.data.token);
+        history.push("/howtos");
+      })
+      .catch((err) => console.log(err.message));
+  };
 
-    }]);
- 
-   
-    const[loginUser,setLoginUser]= useState({
-        userName:'',
-        password:'',
+  return (
+    <div className="auth-page">
+      <h1>Please Login</h1>
+      <form onSubmit={handleSubmit}>
 
-    })
+        <Group>
+          <label htmlFor="username">User Name</label>
+          <input
+            type="text"
+            id="userName"
+            name="userName"
+            value={userInfo.username}
+            onChange={handleChange}
+          />
+        </Group>
+        <Group>
+          <label htmlFor="password">Password</label>
+          <input
+            type="text"
+            id="password"
+            name="password"
+            value={userInfo.password}
+            onChange={handleChange}
+          />
+          {/* {errors.message.length>0?<p className= 'error'>{errors.message}</p>:null} */}
+        </Group>
 
-    const[errors,setErrors] = useState({
-
-        userName:'',
-        password:'',
-        message:''
-    })
-
-    const logFormSchema = yup.object().shape({
-        userName: yup.string().required('must enter user name'),
-        password: yup.string().required('must enter password')
-    })
-
-    const handleChange= event=>{
-       
-        setLoginUser({...loginUser,[event.target.name]:event.target.value})
-        console.log('user Login in Login form has changed',loginUser)
-    }
-
-    //if credentials are correct grant access to dashboard
-
-    const grantAccess = ()=>{
-        users.map(user=>{
-            if(user.userName===loginUser.userName && user.password===user.password){
-                return (<Dashboard user = {loginUser}/>)
-            }else{
-                return setErrors('You entered invalid user Name or Password')
-            }
-            
-        })
-
-    }
-  
-
-
-    return(
-<>
-
-            <form onSubmit={grantAccess}>
-            <label htmlFor='name'>Name</label>
-            <input type='text' id='userName' name ='userName' value={loginUser.userName} onChange={handleChange}/>
-                <label htmlFor='password'>Password</label>
-                <input type='text' id='password' name ='password' value={loginUser.password} onChange={handleChange}/>
-
-                {errors.message.length>0?<p className= 'error'>{errors.message}</p>:null}
-             </form>
-            <div className='navs'>
-                <Link to = '/marketingPage'>Home</Link>
-            </div>
-            <h1>Welcome to Sign in to How Tos</h1>
-            <form onSubmit={grantAccess}>
-                <Group>
-                    <label htmlFor='userName'>User Name</label>
-                    <input type='text' id='userName' name ='userName' value={loginUser.userName} onChange={handleChange}/>
-                </Group>
-                <Group>
-                    <label htmlFor='password'>Password</label>
-                    <input type='text' id='password' name ='password' value={loginUser.password} onChange={handleChange}/>
-                    {/* {errors.message.length>0?<p className= 'error'>{errors.message}</p>:null} */}
-                </Group>
-
-                <button>Log in</button>
-            </form>
-            </>
-    //         {/* <div className='button-container'>
-              
-    //            <span> If you don't have acoount register here <Link to='/signup'>sign up </Link></span> 
-    // </div> */}
-    )}
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
